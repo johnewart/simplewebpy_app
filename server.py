bafb1ecf422a23892ad9b7d12c49987b6fa6c084
John Ewart <john@unixninjas.org>
2014-06-14 19:11:05 -0700

    Create table

--- server.py
+++ server.py
@@ -11,6 +11,16 @@ class index:
         return render.base(view.listing())
 
     if __name__ == "__main__":
-        app = web.application(urls, globals())
-        app.internalerror = web.debugerror
-        app.run()
+      try:
+        config.DB.query("CREATE TABLE items (id serial primary key,body text)")
+        config.DB.query("INSERT INTO items(body) values('Item 1')")
+        config.DB.query("INSERT INTO items(body) values('Item 2')")
+        config.DB.query("INSERT INTO items(body) values('Item 3')")
+        config.DB.query("INSERT INTO items(body) values('Item 4')")
+        config.DB.query("INSERT INTO items(body) values('Item 5')")
+      except:
+        print "Already created data..."
+
+      app = web.application(urls, globals())
+      app.internalerror = web.debugerror
+      app.run()
